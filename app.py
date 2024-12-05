9a0d21db20eab1ffeb753169f10d32ae66e1a8e5
omri <omrikofialkov@gmail.com>
2024-12-05 21:59:37 +0200

    added dog images to flask after tested all works.

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ app = Flask(__name__)
 
 # list of cat images
 images = [
-    "https://www.pawlovetreats.com/cdn/shop/articles/pembroke-welsh-corgi-puppy_2000x.jpg?v=1628638716"
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcuqUINWHLTIq9D-GWb9gyGG3AWQmO2HiA3w&s",
+    "https://www.pawlovetreats.com/cdn/shop/articles/pembroke-welsh-corgi-puppy_1000x.jpg?v=1628638716",
+    "https://hips.hearstapps.com/goodhousekeeping/assets/17/30/pembroke-welsh-corgi.jpg",
+    "https://img.freepik.com/free-photo/portrait-cute-boxer-dog_181624-47633.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlO4erNhFuKmV1TNly5fu8RbSFftERnpUCUg&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0qDKYT9QCq6VnNu4Rlo-rCzD5CDpwt4JCBvIJ5dhI1NpNjyE0tQl740Kf5lUWwe8T6UA&usqp=CAU"
 ]
 @app.route("/")
 def index():
