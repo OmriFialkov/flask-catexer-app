98d2ca7c3a134ed441181a445689b10fe31b3663
omri <omrikofialkov@gmail.com>
2024-12-08 17:47:24 +0200

    added env variable "PORT" to change port at env file only when i want to change for whole project the port which flask will run on. updated docker-compose, app.py, env.

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def index():
     return render_template("index.html", url=url)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000))) # if port env var is blank port will be 5000.
